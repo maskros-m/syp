174b6f456eaac2d735158f51469defc774f2cfd1
Mi Do <96797434+maskros-m@users.noreply.github.com>
2022-03-27 20:54:15 +0200

    Update messenger.php

--- messenger/messenger.php
+++ messenger/messenger.php
@@ -49,7 +49,7 @@
 		// $state = 1; // return to lowercase. Disable this if CAPSLOCK function is more desirable.
 	    }
 	    else if (isSpace($inputVal) && strlen($msg) != 0) { $msg .= ' ';
-	    	// same as above
+	    	// $state = 1;	// same as above
 	    }
 	    else if (isSpace($inputVal) && strlen($msg) == 0) { $state = 0; } //WhatsApp's minor inconsistency
 	    else if (isShift($inputVal)) { $state = 1;
